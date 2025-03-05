08c9d81bf659991b290a8609ce0fa3987cda5f12
WLXinYuan <63536345+WLXinYuan@users.noreply.github.com>
2025-03-05 22:43:23 +0800

    Update home_page_commuter.dart

--- frontend/lib/pages/home_page_commuter.dart
+++ frontend/lib/pages/home_page_commuter.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class HomePageCommuter extends StatefulWidget {
+  const HomePageCommuter({super.key});
+
   @override
   State<HomePageCommuter> createState() => _HomePageCommuterState();
 }
@@ -27,11 +29,11 @@ class _HomePageCommuterState extends State<HomePageCommuter> {
     }
   }
 
-  final List<Widget> widgetOptions = const <Widget>[
-    HomeNav(),
+  final List<Widget> widgetOptions = <Widget>[
+    const HomeNav(),
     BookingNav(),
     TicketNav(),
-    AccountNav(),
+    const AccountNav(),
   ];
 
   @override
