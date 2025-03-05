36f92d49167d53c1a58a0bb80a01a85eee2ea535
Joey Lim <jyorin0206@gmail.com>
2025-03-05 14:26:01 +0800

    added map dependency and displayed map on the screen

--- frontend/lib/pages/DriverNav/ride_nav.dart
+++ frontend/lib/pages/DriverNav/ride_nav.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class RideNav extends StatefulWidget {
   const RideNav({super.key});
@@ -8,14 +10,27 @@ class RideNav extends StatefulWidget {
 }
 
 class _RideNavState extends State<RideNav> {
+  final mapController = MapController();
+  final options = MapOptions(
+    initialCenter: LatLng(1.3521, 103.8198),
+    initialZoom: 13.0
+
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Text("Ride Testing")
-        ],
-      ),
+      body: FlutterMap(
+          mapController: mapController,
+          options: options,
+          children: [
+            TileLayer(
+              urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+              userAgentPackageName: 'com.example.app',
+              maxZoom: 19,
+            )
+          ]),
+
     );
   }
 }
