763aa646598d04ef8171db4f7bf234aa78874f24
Joey Lim <jyorin0206@gmail.com>
2025-03-29 21:39:31 +0800

    implemented ETA but need to test with backend first

--- frontend/lib/models/RouteResponse.dart
+++ frontend/lib/models/RouteResponse.dart
@@ -1,10 +1,12 @@
 import 'package:latlong2/latlong.dart';
 
 class RouteResponse {
+  final int duration;
   final String polyline;
   final List<LatLng> decodedRoute;
 
   RouteResponse({
+    required this.duration,
     required this.polyline,
     required this.decodedRoute,
   });
@@ -13,6 +15,7 @@ class RouteResponse {
   factory RouteResponse.fromJson(Map<String, dynamic> json) {
     print(json["decodedRoute"]);
     return RouteResponse(
+      duration: json["duration"],
       polyline: json['polyline'],
       decodedRoute: (json['decodedRoute'] as List)
           .map((coords) => LatLng(coords[0] as double, coords[1] as double))
