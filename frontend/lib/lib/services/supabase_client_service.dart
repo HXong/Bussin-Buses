bb7962e940090cbfd259feee833dd24383caf568
Joey Lim <jyorin0206@gmail.com>
2025-04-06 21:52:17 +0800

    remove nested lib

--- frontend/lib/lib/services/supabase_client_service.dart
+++
@@ -1,8 +0,0 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
-class SupabaseClientService {
-  static final SupabaseClient _supabase = Supabase.instance.client;
-
-  // Expose the Supabase client
-  static SupabaseClient get client => _supabase;
-}
